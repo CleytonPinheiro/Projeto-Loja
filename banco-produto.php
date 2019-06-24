0ee16cea2f3d041e2f2ce3158047311641764913
CleytonPinheiro <42612444+CleytonPinheiro@users.noreply.github.com>
2019-06-23 22:19:45 -0300

    Adicionado opção de alteração de produtos
    Adicionado opção de alteração de produtos, trazendo as informações do referente do produto preenchidos p/ alterações.

--- banco-produto.php
+++ banco-produto.php
@@ -1,23 +1,38 @@
 <?php
 
-function listaProdutos ($conexao){
- $produtos = array ();
- $resultado = mysqli_query ($conexao, "select * from produtos");
+function listaProdutos($conexao){
 
- while ($produto = mysqli_fetch_assoc ($resultado)) {
-  	array_push ($produtos, $produto);
+$produtos = array();
+
+$resultado = mysqli_query($conexao, "select p.*, c.nome as categoria_nome from
+	produtos as p join categorias as c on p.categoria_id = c.id");
+
+while($produto = mysqli_fetch_assoc($resultado)) {
+    array_push($produtos, $produto);
+}
+	return $produtos;
+}
+
+function insereProduto($conexao, $nome, $preco, $descricao,$categoria_id, $usado) {
+    $query = "insert into produtos (nome, preco, descricao, categoria_id,usado) 
+    values ('{$nome}', {$preco},'{$descricao}',{$categoria_id}, {$usado})";
+    return mysqli_query($conexao, $query);
+}
+
+function alteraProduto($conexao,$id,$nome,$preco,$descricao,$categoria_id,$usado){
+    $query="update produtos set nome='{$nome}',preco= {$preco}, descricao='{$descricao}', categoria_id={$categoria_id}, usado={$usado} where id= '{$id}'";
+        return mysqli_query($conexao, $query);
 
 }
-return $produtos;
 
-}
+function buscaProduto($conexao, $id){
+	$query = "select * from produtos where id = {$id}";
+	$resultado = mysqli_query($conexao,$query);
+	return mysqli_fetch_assoc($resultado);
 
-function insereProduto ($conexao,$nome,$preço){
-	$query = "insert into produtos (nome,preco) values ('{$nome}',{$preço})";
-	return mysqli_query ($conexao, $query);
 }
 
 function removeProduto($conexao, $id){
-	$query = "delete from produtos where id= {$id}";
+	$query="delete from produtos where id = {$id}";
 	return mysqli_query ($conexao, $query);
 }
